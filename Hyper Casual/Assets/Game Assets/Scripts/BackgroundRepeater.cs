b3f715f5ebba730d02ec5c9689adca45fc5ed2ca
Lucas Fernandes <84829810+notluxca@users.noreply.github.com>
2025-01-23 23:36:37 -0300

    :update: Current state of background repetition and new shader tweaks

--- Hyper Casual/Assets/Game Assets/Scripts/BackgroundRepeater.cs
+++ Hyper Casual/Assets/Game Assets/Scripts/BackgroundRepeater.cs
@@ -1,34 +1,66 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class BackgroundRepeater : MonoBehaviour
 {
-    public Transform player;
+    // public Transform player;
     public float threshold = 10f;
+    public GameObject middleTile;
+    public Transform player;
 
-    private Transform firstChild;
-    private Transform secondChild;
-    private Transform temp;
+    public List<GameObject> children = new List<GameObject>();
+    int currentIndex = 0;
+    int currentTilePosition = 3;
 
     private void Start()
     {
-        firstChild = transform.GetChild(0);
-        secondChild = transform.GetChild(1);
+        player = GameObject.Find("Player").transform;
+        foreach (Transform child in transform)
+        {
+            // children.Add(child.gameObject); // adiciona as 3 childs
+        }
+        middleTile = GetMiddleTilePosition(children[0], children[1], children[2]);
+        
     }
 
     private void Update()
     {
-        if (player.position.z + threshold > firstChild.position.z)
-        {
-            TeleportFirstChild();
-        }
+        PlayerPositionDetection();
     }
 
-    private void TeleportFirstChild()
+    
+
+    public GameObject GetMiddleTilePosition(GameObject first, GameObject second, GameObject third)
     {
-        var firstChildSize = firstChild.localScale.z;
-        var secondChildSize = secondChild.localScale.z;
-        var pos = firstChild.position;
-        pos.z = secondChild.position.z + (secondChildSize + firstChildSize);
-        firstChild.position = pos;
+        var list = new List<GameObject> { first, second, third };
+        list.Sort((a, b) => a.transform.position.x.CompareTo(b.transform.position.x));
+        return list[1];
     }
+
+    public void PlayerPositionDetection(){
+        Debug.Log($"{transform.position.x}");
+        if(player.position.z > middleTile.transform.position.x - middleTile.transform.parent.position.x){
+            TeleportChild(); 
+        }
+    }
+    
+    
+    public void TeleportChild(){
+        children[currentIndex].transform.localPosition = new Vector3(currentTilePosition, 0, 0);
+            currentIndex++;
+            currentTilePosition++;
+            if(currentIndex > 2){
+                currentIndex = 0;
+            }
+            middleTile = GetMiddleTilePosition(children[0], children[1], children[2]);
+    }
+
+    // detect who is the midle plataform
+    // once player is geting close to the midle plataform, teleport the first plataform to the last plataform
+
+    //observar posição x de cada um
+    // através da posição x determinar ordem retornar tile do meio 
+    // 
+    
 }
--- Hyper Casual/Assets/Game Assets/Scripts/Plataform/Plataform.cs
+++ Hyper Casual/Assets/Game Assets/Scripts/Plataform/Plataform.cs
@@ -14,8 +14,24 @@ public class Plataform : MonoBehaviour
     void Update()
     {
         if(transform.position.z + distanceToFall < mainCamera.transform.position.z){
-            rb.constraints = RigidbodyConstraints.None;
-            rb.linearVelocity = new Vector3(0, -0.8f, 0);
+            Fall();
         }
+    }
+
+        /// <summary>
+        /// Triggered when the player hits the platform.
+        /// If the player hits the platform, it starts falling.
+        /// </summary>
+        /// <param name="other">The collision object</param>
+    private void OnCollisionEnter(Collision other) {
+        if(other.gameObject.CompareTag("Player")){
+            Debug.Log($"Player hit the platform {gameObject.name}");
+            GetComponent<Animator>().SetTrigger("Fall");
+        }
+    }
+
+    public void Fall(){
+        rb.constraints = RigidbodyConstraints.None;
+        rb.linearVelocity = new Vector3(0, -0.8f, 0);
     }
 }   
